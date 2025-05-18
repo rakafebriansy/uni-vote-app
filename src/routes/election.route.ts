02d02a6f5a5d2ffb0cd60f63cce46d43864245ad
rakafebriansy <rakafebrian19@gmail.com>
2025-05-18 07:43:34 +0700

    fix: add is admin middleware into delete election  route

--- src/routes/election.route.ts
+++ src/routes/election.route.ts
@@ -10,6 +10,6 @@ router.use(authenticate as RequestHandler);
 router.post('/', isAdmin as RequestHandler, create as any);
 router.get('/', search as any);
 router.get('/:id', get as any);
-router.delete('/:id', remove as any);
+router.delete('/:id', isAdmin as RequestHandler, remove as any);
 
 export default router;
